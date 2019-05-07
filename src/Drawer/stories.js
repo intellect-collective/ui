c3b3e4bed99fa22b84ce9f80f76a5abbdc55b21d
Thomas Wilson <thomas@thomaspwilson.com>
2019-05-07 14:36:30 -0400

    Fix drawer story

--- src/Drawer/stories.js
+++ src/Drawer/stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import classnames from 'classnames';
 import { storiesOf } from '@storybook/react';
+import { withState } from '@dump247/storybook-state';
 import { Drawer, List, ListItem } from '../..';
 
 const Menu = () => (
@@ -16,9 +18,39 @@ const Menu = () => (
 	</List>
 );
 
+const ref = React.createRef();
+
+const set = (store, update) => {
+	if (update.anchor !== store.state.anchor && store.state.open) {
+		store.set({ open: false });
+
+        ref.current.addEventListener('transitionend', function f(e) {
+            ref.current.removeEventListener('transitionend', f);
+            store.set({ anchor: update.anchor });
+            setTimeout(() => {
+	            requestAnimationFrame(() => {
+	            	requestAnimationFrame(() => {
+	            		store.set({ ...update, open: true });
+	            	})
+	            }),
+	            250
+            })
+
+        });
+	} else {
+		store.set(update)
+	}
+}
+
 storiesOf('Drawer', module)
-    .add('default', () => (
-        <Drawer classes="docked anchor-left">
-            <Menu />
-        </Drawer>
-    ));
+    .add('default', withState({ anchor: 'anchor-left' })(({ store }) => (
+    	<div>
+	        <Drawer classes={ classnames('docked', store.state.anchor, { open: store.state.open }) } open ref={ ref }>
+	            <Menu />
+	        </Drawer>
+	        <button className="btn btn-primary" onClick={ () => { set(store, { anchor: 'anchor-left', open: !store.state.open }) } }>Left</button>
+	        <button className="btn btn-primary" onClick={ () => { set(store, { anchor: 'anchor-top', open: !store.state.open }) } }>Top</button>
+	        <button className="btn btn-primary" onClick={ () => { set(store, { anchor: 'anchor-right', open: !store.state.open }) } }>Right</button>
+	        <button className="btn btn-primary" onClick={ () => { set(store, { anchor: 'anchor-bottom', open: !store.state.open }) } }>Bottom</button>
+        </div>
+    )));
